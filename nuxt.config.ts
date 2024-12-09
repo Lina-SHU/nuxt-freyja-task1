441394f5d14657bd0ab981ea784d9145267b9c84
LinaChen <sakura0477@gmail.com>
2024-12-09 23:35:12 +0800

    feat: add images, layout, templates, plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '@/assets/stylesheets/all.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+          additionalData: `@import "bootstrap/scss/functions"; @import "bootstrap/scss/variables";`
+        }
+      }
+    }
+  },
+  router: {
+    options: {
+      linkActiveClass: 'active',
+      linkExactActiveClass: 'active'
+    }
+  }
 })
